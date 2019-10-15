21ce7acc5c81fc52d74d03f2608e4be7658c4b55
drmo05 <56580257+drmo05@users.noreply.github.com>
2019-10-15 19:52:17 +0900

    Update sketch_oct15a.ino

--- Advanced/Serial/sketch_oct15a/sketch_oct15a.ino
+++ Advanced/Serial/sketch_oct15a/sketch_oct15a.ino
@@ -1,10 +1,12 @@
 void setup() {
   Serial.begin(9600);
+  pinMode(3,OUTPUT);
 }
 
 void loop() {
   if (Serial.available()) {
     long value = Serial.parseInt();
     Serial.println(value);
+    analogWrite(3,value);
   }
 }
